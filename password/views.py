97b0b9b9848a8133576e9220e6c01afd26b94777
hilal <hilalkhattak@yahoo.com>
2020-11-29 15:46:37 +0000

    commit

--- password/views.py
+++ password/views.py
@@ -7,13 +7,24 @@ import random
 def home(request):
     return render(request, 'passwordapp/home.html')
 
-def pwd(request):
 
-    
+def pwd(request):
 
     characters = list('abcdefghijklmnopqrstuvwxy')
 
-    length = int(request.GET.get('length'))
+   
+
+    if request.GET.get('Numbers'):
+        characters.extend(list('0987654321'))
+        
+    if request.GET.get('UpperCase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+   
+    if request.GET.get('characters'):
+        characters.extend(list('!@£$%^&*'))                
+
+    length = int(request.GET.get('length', 7))
 
     thepasssword = ''
     for x in range(length):
