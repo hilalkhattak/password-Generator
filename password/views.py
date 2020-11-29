041087619e1ab0ea36b5893fab0dec23a1f28820
hilal <hilalkhattak@yahoo.com>
2020-11-29 15:24:15 +0000

    commit

--- password/views.py
+++ password/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import random
+
 
 def home(request):
     return render(request, 'passwordapp/home.html')
 
 def pwd(request):
-    return render(request, 'passwordapp/pwd.html')  
+
+    
+
+    characters = list('abcdefghijklmnopqrstuvwxy')
+
+    length = int(request.GET.get('length'))
+
+    thepasssword = ''
+    for x in range(length):
+        thepasssword += random.choice(characters)
+
+    return render(request, 'passwordapp/pwd.html', {'pwd':thepasssword})  
